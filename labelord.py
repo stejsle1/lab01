88be74bdb81aa7ea1ccfa0272a63e1d1afe2ebe1
[stejsle1] <[stejsle1@fit.cvut.cz]>
2017-10-19 11:09:30 +0200

    hotovo, otestovano - fail 4 testy

--- labelord.py
+++ labelord.py
@@ -5,7 +5,7 @@ import configparser
 import json
 import sys
 import os
-
+            
 def setup(session, token):
    session.headers = {'User-Agent': 'Python'}
    def token_auth(req):
@@ -30,27 +30,30 @@ def printextra(level, text, label, err):
          print('[' + label + '][ERR] ' + text)               
       if err == 2:
          print('[' + label + '][DRY] ' + text)
-   if level == 4:
-      if err == 2:
+   if level > 4:
+      if level == 6:
          print('[SUMMARY] ' + text) 
-      else:
+      if level == 5:
          print('SUMMARY: ' + text)         
 
 # Structure your implementation as you want (OOP, FP, ...)
 # Try to make it DRY also for your own good
 
 
-@click.group('labelord') 
+@click.group('labelord')                
+@click.help_option('-h', '--help')
+@click.version_option(version='labelord, version 0.1')
 @click.option('-c', '--config', type=click.Path(exists=True), help='Config file')
 @click.option('-t', '--token', help='Token')
 @click.option('--tenv', envvar='GITHUB_TOKEN')
 @click.pass_context
 def cli(ctx, config, token, tenv):
-   conffile = configparser.ConfigParser()
    
+   conffile = configparser.ConfigParser()
+             
    if config is not None:
       conffile.read(config) 
-   else:
+   else: 
       if os.path.isfile('./config.cfg') == True:
          conffile.read('./config.cfg')
          config = './config.cfg'
@@ -78,11 +81,11 @@ def cli(ctx, config, token, tenv):
    ctx.obj['config'] = config
 
 
-@cli.command()
+@cli.command()   
 @click.pass_context
 def list_repos(ctx):
-   
-   session = ctx.obj['session']
+   """List repos."""
+   session = ctx.obj['session'] 
 
    repos = session.get('https://api.github.com/user/repos?per_page=100&page=1')
    a = 0
@@ -110,11 +113,11 @@ def list_repos(ctx):
 
 
 @cli.command()
-@click.argument('repository', required=1)
+@click.argument('repository', required=1) 
 @click.pass_context
 def list_labels(ctx, repository):
-
-   session = ctx.obj['session']
+   """List labels."""
+   session = ctx.obj['session'] 
    a = 0
    list = session.get('https://api.github.com/repos/' + repository + '/labels?per_page=100&page=1')
    if list.status_code == 404:
@@ -146,13 +149,13 @@ def list_labels(ctx, repository):
            a = a+1
           
 
-@cli.command()
+@cli.command()                      
 @click.argument('mode', type=click.Choice(['update', 'replace']))
 @click.option('-r', '--template-repo', help="Add a template repo.")
 @click.option('-a', '--all-repos', is_flag=True, help='All available repos.')
 @click.option('-d', '--dry-run', is_flag=True, help='Dry run')
 @click.option('-v', '--verbose', is_flag=True, help='Verbose mode')
-@click.option('-q', '--quiet', is_flag=True, help='Quiet mode')
+@click.option('-q', '--quiet', is_flag=True, help='Quiet mode') 
 @click.pass_context
 def run(ctx, mode, template_repo, all_repos, dry_run, verbose, quiet):
    
@@ -176,7 +179,7 @@ def run(ctx, mode, template_repo, all_repos, dry_run, verbose, quiet):
             if conffile.getboolean('repos', repo):
                repos.append(repo)
    else: 
-      reposlist = session.get('https://api.github.com/user/repos')
+      reposlist = session.get('https://api.github.com/user/repos?per_page=100&page=1')
       if 'message' in reposlist.json() and reposlist.json()['message'] == 'Bad credentials':
          print("GitHub: ERROR " + str(reposlist.status_code) + ' - ' + reposlist.json()['message'], file=sys.stderr)
          sys.exit(4)
@@ -192,6 +195,7 @@ def run(ctx, mode, template_repo, all_repos, dry_run, verbose, quiet):
    labels = {}
    ok = 0
    level = 1
+   error_code = 0
    if verbose: level = 2
    if quiet: level = 0
    if verbose and quiet: level = 1
@@ -210,7 +214,7 @@ def run(ctx, mode, template_repo, all_repos, dry_run, verbose, quiet):
                labels[label] = conffile['labels'][label]      
       else: 
          # update template repo z configu
-         list = session.get('https://api.github.com/repos/' + conffile['others']['template-repo'] + '/labels')
+         list = session.get('https://api.github.com/repos/' + conffile['others']['template-repo'] + '/labels?per_page=100&page=1')
          if list.status_code == 404: 
             printextra(level, conffile['others']['template-repo'] + '; ' + str(list.status_code) + ' - ' + list.json()['message'], 'LBL', 1)
             errors = errors + 1
@@ -218,7 +222,7 @@ def run(ctx, mode, template_repo, all_repos, dry_run, verbose, quiet):
             labels[label['name']] = label['color']
    else: 
       # update --template-repo z prepinace
-      list = session.get('https://api.github.com/repos/' + template_repo + '/labels')
+      list = session.get('https://api.github.com/repos/' + template_repo + '/labels?per_page=100&page=1')
       if list.status_code == 404: 
          printextra(level, template_repo + '; ' + str(list.status_code) + ' - ' + list.json()['message'], 'LBL', 1)
          errors = errors + 1
@@ -230,16 +234,18 @@ def run(ctx, mode, template_repo, all_repos, dry_run, verbose, quiet):
       list = session.get('https://api.github.com/repos/' + repo + '/labels?per_page=100&page=1') 
       if list.status_code != 200:
          printextra(level, repo + '; ' + str(list.status_code) + ' - ' + list.json()['message'], 'LBL', 1)
+         error_code = 10
+         errors = errors + 1
+         continue
       for label in list.json():
          if label['name'] in labels:
-            if labels[label['name']] != label['color']: 
+            if labels[label['name']] != label['color']:
                colors = json.dumps({"name": label['name'], "color": labels[label['name']]})
-               c = c + 1
-               if not dry_run: req = session.patch('https://api.github.com/repos/' + repo + '/labels/' + label['name'], data=colors) 
+               if not dry_run: req = session.patch('https://api.github.com/repos/' + repo + '/labels/' + label['name'].lower(), data=colors) 
                if dry_run or req.status_code == 200:
-                  printextra(level, repo + '; ' + label['name'] + '; ' + labels[label['name']] + '; ' + label['name'] + '; ' + labels[label['name']], 'UPD', err)
+                  printextra(level, repo + '; ' + label['name'] + '; ' + labels[label['name']], 'UPD', err)
                else:
-                  printextra(level, repo + '; ' + str(req.status_code) + ' - ' + req.json()['message'], 'UPD', 1)
+                  printextra(level, repo + '; ' + label['name'] + '; ' + labels[label['name']] + '; ' + str(req.status_code) + ' - ' + req.json()['message'], 'UPD', 1)
                   errors = errors + 1
          elif mode == 'replace':
             if not dry_run: req = session.delete('https://api.github.com/repos/' + repo + '/labels/' + label['name'])
@@ -250,23 +256,24 @@ def run(ctx, mode, template_repo, all_repos, dry_run, verbose, quiet):
                errors = errors + 1
       for label in labels:
          for label2 in list.json():
-            if label == label2: ok = 1
+            if label == label2['name']: ok = 1
          if ok != 1: 
-            colors = json.dumps({"name":label, "color":labels[label]})
-            if not dry_run: req = session.post('https://api.github.com/repos/' + repo + '/labels', data=colors)  
-            if c > 0: sys.exit(sum)
+            colors = json.dumps({"name": label, "color": labels[label]})
+            if not dry_run: req = session.post('https://api.github.com/repos/' + repo + '/labels', data=colors) 
             if dry_run or req.status_code == 201:
                printextra(level, repo + '; ' + label + '; ' + labels[label], 'ADD', err)
             else:
                printextra(level, repo + '; ' + label + '; ' + labels[label] + '; ' + str(req.status_code) + ' - ' + req.json()['message'], 'ADD', 1)
                errors = errors + 1
+               error_code = 10
                        
          ok = 0
    if errors != 0:      
-      printextra(4, str(errors) + ' error(s) in total, please check log above', '', level)      
+      printextra(4+level, str(errors) + ' error(s) in total, please check log above', '', err)      
    else:
-      printextra(4, str(sum) + ' repo(s) updated successfully', '', level)    
-         
+      printextra(4+level, str(sum) + ' repo(s) updated successfully', '', err)    
+   
+   sys.exit(error_code)      
 
 if __name__ == '__main__':
     cli(obj={})
